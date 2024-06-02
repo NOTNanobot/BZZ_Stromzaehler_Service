c753d4b25772a4adcc5992205a78ff08ef03e1dc
NOTNanobot <nanobot5.play@gmail.com>
2024-06-02 13:49:59 +0200

    init

---
+++ src/main/java/com/nanobot/bzzstromzaehlerservice/BzzStromzaehlerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nanobot.bzzstromzaehlerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzzStromzaehlerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BzzStromzaehlerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanobot/bzzstromzaehlerservice/BzzStromzaehlerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanobot.bzzstromzaehlerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BzzStromzaehlerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
